66c95e66e864d9dd306ca2853d55f214970b2632
hyeonbin jeong <duafuuu@gmail.com>
2023-07-10 23:49:12 +0900

    style: 변수명 및 코드, 주석 수정

--- index.js
+++ index.js
@@ -2,112 +2,119 @@ const form = document.querySelector('form');
 const input = document.querySelector('input');
 const ul = document.querySelector('ul');
 const clear = document.querySelector('.clear');
+let todos = []; // 할 일 목록을 저장하는 배열
 
-// item을 로컬스토리지에 저장하기
-let todos = [];
-
-const save = () => {
+// 로컬 스토리지에 todos 배열을 저장
+const saveTodo = () => {
   localStorage.setItem('todos', JSON.stringify(todos));
 };
 
-// 버튼의 부모요소 찾고 target에 저장한 뒤에 문서에서 삭제하기
-const delItem = (event) => {
+// 할 일 목록을 삭제
+const delTodo = (event) => {
   const target = event.target.parentElement;
 
   // todos 배열에서 해당 id값을 가진 요소를 찾아서 삭제하고 그 요소가 삭제된 새로운 배열을 다시 저장
   todos = todos.filter((todo) => todo.id !== parseInt(target.id));
-  save();
+  saveTodo();
 
   target.remove();
 };
 
-// 전체 삭제
+// 할 일 목록 전체 삭제
 const allClear = () => {
   localStorage.clear('todos', JSON.stringify(todos));
-  let ul = (document.querySelector('ul').innerHTML = '');
+  ul.innerHTML = '';
 };
 
-// 입력받은 text로 취급하는 input 값이 비어있지 않으면, html의 새로운 요소(li)로 만들어 문서에 추가
-const addItem = (todo) => {
+// 할 일 추가하고, 화면에 표시
+const addTodo = (todo) => {
   if (todo.text !== '') {
-    const li = document.createElement('li');
+    const li = document.createElement('li'); //새로운 li 요소를 생성
+    const button = document.createElement('button'); // 삭제 버튼을 생성
+    const span = document.createElement('span'); // 할 일 텍스트를 감싸기 위한 span 요소를 생성
+    const check = document.createElement('button'); // 완료 체크 버튼을 생성
 
-    // text를 별도의 span으로 묶기 위해 생성
-    // 개별 삭제, 체크 button 생성
-    const button = document.createElement('button');
-    const span = document.createElement('span');
-    const check = document.createElement('button');
+    check.innerText = '✔'; // 완료 체크 버튼의 텍스트를 설정
+    check.classList.add('btn-check'); // 완료 체크 버튼에 CSS 클래스를 추가
+    span.innerText = todo.text; // 할 일 텍스트를 설정
+    button.innerText = '✘'; // 삭제 버튼의 텍스트를 설정
+    button.classList.add('btn-x'); // 삭제 버튼에 CSS 클래스를 추가
 
-    check.innerText = '✔';
-    check.classList.add('btn-check');
-    span.innerText = todo.text;
-    button.innerText = '✘';
-    button.classList.add('btn-x');
+    button.addEventListener('click', delTodo); // 삭제 버튼에 클릭 이벤트 리스너를 추가
+    clear.addEventListener('click', allClear); // 전체 삭제 버튼에 클릭 이벤트 리스너를 추가
 
-    // 버튼에 이벤트 달기
-    button.addEventListener('click', delItem);
-    clear.addEventListener('click', allClear);
+    // 할 일 완료 버튼을 클릭했을 때, 완료 상태를 토글하고 저장
     check.addEventListener('click', () => {
       li.classList.toggle('complete');
+      todo.completed = !todo.completed; // 완료 상태를 반전시킴
+      saveTodo();
     });
 
     // li 요소의 자식으로 구성
-    li.appendChild(check);
-    li.appendChild(span);
-    li.appendChild(button);
-    ul.appendChild(li);
+    li.appendChild(check); // li 요소에 완료 체크 버튼을 추가
+    li.appendChild(span); // li 요소에 할 일 텍스트를 감싼 span 요소를 추가
+    li.appendChild(button); // li 요소에 삭제 버튼을 추가
+    ul.appendChild(li); // ul 요소에 li 요소를 추가
 
-    // 생성한 리스트 요소의 id값을 todo가 가지고 있는 id값으로 지정
+    // li 요소의 id 속성에 todo가 가지고 있는 고유 ID를 설정
     li.id = todo.id;
+
+    // 로컬 스토리지에서 완료된 항목인 경우, 클래스를 추가하여 줄 그어진 상태로 표시
+    if (todo.completed) {
+      li.classList.add('complete');
+    }
   }
 };
 
-// submit 이벤트가 일어날 때 preventDefault를 통해 페이지가 새로고침 안 되게 하기
-const handler = (event) => {
+// 폼 제출 이벤트 핸들러
+const handleSubmit = (event) => {
   event.preventDefault();
 
-  // todo라는 객체로 포장하기
+  // 입력된 할 일을 todo 객체로 포장
   const todo = {
     id: Date.now(),
     text: input.value,
+    completed: false, // 새로운 할 일은 완료되지 않은 상태로 초기화
   };
 
-  // todos라는 배열에 추가
+  // todos 배열에 추가
   todos.push(todo);
 
-  // addItem을 호출하면서 input 값 넘겨주기
-  addItem(todo);
+  // 화면에 할 일을 추가
+  addTodo(todo);
 
-  // 저장 호출
-  save();
+  // todos 배열을 저장
+  saveTodo();
 
-  // input 창을 공백으로 초기화
+  // 입력 필드를 초기화
   input.value = '';
 };
 
-// 새롭게 페이지를 로드할 때 만약에 로컬스토리지에 저장된 기본 정보가 있다면 해당 정보를 알아서 생성해서 나오도록 하기
+// 초기화 함수
 const init = () => {
   const userTodos = JSON.parse(localStorage.getItem('todos'));
 
   if (userTodos) {
+    // 새롭게 페이지를 로드할 때 만약에 로컬스토리지에 저장된 기본 정보가 있다면 해당 정보를 알아서 생성해서 나오도록 하기
     userTodos.forEach((todo) => {
-      addItem(todo);
+      addTodo(todo);
     });
 
     todos = userTodos;
   }
 };
 
+// 초기화 함수를 실행하여 이전에 저장된 할 일 목록을 화면에 표시
 init();
 
-// form이 submit 이벤트가 일어날 때, handler 함수 동작
-form.addEventListener('submit', handler);
+// form이 submit 이벤트가 일어날 때, handleSubmit 함수 동작
+form.addEventListener('submit', handleSubmit);
 
 // Date() 오늘 날짜 가져오기
 const today = new Date();
+const formattedDate = `${today.getFullYear()}.${String(today.getMonth() + 1).padStart(
+  2,
+  '0'
+)}.${String(today.getDate()).padStart(2, '0')}`;
 
-const year = today.getFullYear();
-const month = ('0' + (today.getMonth() + 1)).slice(-2);
-const day = ('0' + today.getDate()).slice(-2);
-
-document.getElementById('current-date').innerHTML = year + '.' + month + '.' + day;
+document.getElementById('current-date').textContent = formattedDate;
